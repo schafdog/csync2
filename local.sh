0810a53a13c82440f4260fa043d41e087c80b39a
Dennis schafroth <dennis@schafroth.com>
2013-04-15 02:16:14 +0200

    Configurable command (cfu) and optinal pause

--- local.sh
+++ local.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+COMMAND=x
 function check {
-    ./csync2 -p 30860 -K csync2_local.cfg -N local -cr$DEBUG test/local >> csync_local.log 2>&1
+    ./csync2 -p 30860 -K csync2_local.cfg -N local -${COMMAND}r$DEBUG test/local >> csync_local.log 2>&1
     echo "select * from file; select * from dirty;" | mysql -t -u csync2_local -pcsync2_local csync2_local >> mysql_local.log
+    ${PAUSE}
 }
 
 
 echo "delete from dirty ; delete from file" | mysql -u csync2_local -pcsync2_local csync2_local
 rm csync_local.log mysql_local.log
 rm -rf test/local/*
-check 
+check
 echo first > test/local/first.txt
 check
 mkdir test/local/dir1
