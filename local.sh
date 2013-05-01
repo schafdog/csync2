7c857a56fa768d412ec56704e44292f81c0a0d91
Dennis schafroth <dennis@schafroth.com>
2013-05-01 22:59:50 +0200

    fix softlink. add absolut path softlink

--- local.sh
+++ local.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
-COMMAND=x
+if [ "$1" == "" ] ; then 
+    COMMAND=c
+else
+    COMMAND=$1
+fi
+echo "Running command $COMMAND" 
+
 function check {
     ./csync2 -p 30860 -K csync2_local.cfg -N local -${COMMAND}r$DEBUG test/local >> csync_local.log 2>&1
     echo "select * from file; select * from dirty;" | mysql -t -u csync2_local -pcsync2_local csync2_local >> mysql_local.log
@@ -30,5 +36,7 @@ ln test/local/dir1/third test/local/hardlink
 check
 ln test/local/hardlink test/local/2hardlink
 check
-ln -s test/local/hej.txt test/local/softlink
+ln -s hej.txt test/local/softlink
+check
+ln -s /etc/hosts test/local/absolut_softlink
 check
