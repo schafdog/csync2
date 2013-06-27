ddf9ebb79b53a528aaf8b50b5e53ba6f9dce4517
Dennis Schafroth <dennis@schafroth.com>
2013-06-27 23:48:55 +0200

    Support Darwin stat

--- find_hardlinks.sh
+++ find_hardlinks.sh
@@ -1,3 +1,9 @@
 pushd $1  > /dev/null
-find . -type f -not -links 1 -exec stat -c '%h %n' {} \;  | sort -n 
+OS=`uname`
+FORMAT="-c ' %h %n ' "  
+if [ "$OS" == "Darwin" ] ; then
+    find . -type f -not -links 1 -exec stat -f '%l %N'  {} \;  | sort -n 
+else
+    find . -type f -not -links 1 -exec stat -c '%h %n'  {} \;  | sort -n 
+fi
 popd > /dev/null
