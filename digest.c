564412435c695994a50318bf2a58dd65d5fbfee0
Dennis schafroth <dennis@schafroth.com>
2014-02-23 10:51:50 +0100

    Refactor to library

--- digest.c
+++ digest.c
@@ -3,26 +3,53 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
-#include <openssl/evp.h>
+#include "digest.h"
 
-main(int argc, char *argv[])
+int dsync_digest(int file, const char *digest_name, unsigned char *md_value, unsigned int *md_len) 
 {
   EVP_MD_CTX *mdctx;
   const EVP_MD *md;
-  unsigned char md_value[EVP_MAX_MD_SIZE];
-  int md_len, i;
+  int i; 
 
   OpenSSL_add_all_digests();
 
-  if(!argv[1]) {
-    printf("Usage: digest digestname filename\n");
-    exit(1);
-  }
-
-  md = EVP_get_digestbyname(argv[1]);
+  md = EVP_get_digestbyname(digest_name);
   
   if(!md) {
-    printf("Unknown message digest %s\n", argv[1]);
+    printf("Unknown message digest %s\n", digest_name);
+    return 1;
+  }
+
+  mdctx = EVP_MD_CTX_create();
+  EVP_DigestInit_ex(mdctx, md, NULL);
+  char buffer[4096];
+  ssize_t n;
+  ssize_t total = 0;
+  while ((n = read(file, buffer, 4096)) > 0) {
+    EVP_DigestUpdate(mdctx, buffer, n);
+    total += n;
+    }
+  EVP_DigestFinal_ex(mdctx, md_value, md_len);
+
+  EVP_MD_CTX_destroy(mdctx);
+
+  return 0;
+}
+
+void dsync_digest_hex(const unsigned char *md_value, unsigned int md_len, char *digest_str)
+{
+  for (int i = 0; i < md_len; i++) {
+    sprintf(digest_str+2*i, "%02x", md_value[i]);
+  }
+}
+
+#ifdef DIGEST_STANDALONE
+int main(int argc, char *argv[])
+{
+  unsigned char md_value[EVP_MAX_MD_SIZE];
+  unsigned int md_len = 0, i;
+  if(!argv[1]) {
+    printf("Usage: digest digestname filename\n");
     exit(1);
   }
 
@@ -35,21 +62,16 @@ main(int argc, char *argv[])
   else
     file = STDIN_FILENO;
 
-
-  mdctx = EVP_MD_CTX_create();
-  EVP_DigestInit_ex(mdctx, md, NULL);
-  char buffer[4096];
-  ssize_t n;
-  ssize_t total = 0;
-  while ((n = read(file, buffer, 4096)) > 0) {
-    EVP_DigestUpdate(mdctx, buffer, n);
-    total += n;
-    }
-  EVP_DigestFinal_ex(mdctx, md_value, &md_len);
-  EVP_MD_CTX_destroy(mdctx);
-
-  printf("Digest is (%u): ", total);
-  for(i = 0; i < md_len; i++) 
-    printf("%02x", md_value[i]);
-  printf("\n");
+  int rc = dsync_digest(file, argv[1], md_value, &md_len);
+  if (rc) 
+    printf("Failed to digest %s with %s", argv[2], argv[1]);
+  else {
+    //printf("Digest is (%u): ", total);
+    char hex_str[2*EVP_MAX_MD_SIZE+1]; 
+    dsync_digest_hex(md_value, md_len, hex_str);
+    printf("%s\n", hex_str);
+  }
+  return 0;
 }
+
+#endif
