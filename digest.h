20ccea458999b41233eb72a72d047d9a5eb04e50
Dennis Schafroth <dennis@schafroth.dk>
2014-10-26 12:29:49 +0100

    Include OS X crypto

--- digest.h
+++ digest.h
@@ -1,7 +1,13 @@
 #ifndef DIGEST_H
 #define DIGEST_H
 
+#if __DARWIN_C_LEVEL
+#include <CommonCrypto/CommonDigest.h>
+#define DIGEST_MAX_SIZE 64
+#else
 #include <openssl/evp.h>
+#define DIGEST_MAX_SIZE EVP_MAX_MD_SIZE
+#endif
 
 int dsync_digest(int file, const char *digest_name, unsigned char *md_value, unsigned int *md_len);
 void dsync_digest_hex(const unsigned char *md_value, unsigned int md_len, char *digest_str);
