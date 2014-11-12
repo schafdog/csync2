23c014512d9b081a90ba816763dbff9e84c645a4
Dennis schafroth <dennis@schafroth.com>
2014-11-12 13:53:12 +0100

    Extract global variables

---
+++ global.c
@@ -0,0 +1,16 @@
+
+#include "csync2.h"
+
+int csync_error_count = 0;
+int csync_debug_level = 0;
+FILE *csync_debug_out = 0;
+int csync_syslog = 0;
+
+int csync_server_child_pid = 0;
+int csync_timestamps = 0;
+int csync_new_force = 0;
+
+int csync_dump_dir_fd = -1;
+
+char *csync_port = "30865";
+char myhostname[256] = "";
