958562d29f61be3080c3bfea0d9978d7f4523077
Dennis schafroth <dennis@schafroth.com>
2014-11-13 20:51:56 +0100

    Works with newline

--- tests/test_key_time_remove.c
+++ tests/test_key_time_remove.c
@@ -7,7 +7,7 @@ void conn_remove_key(char *buffer);
 
 int main(int argc, char *argv[]) {
     
-    const char *testSIG = "SIG 9iNlOKBHPfeAtRpsCgaQqTwKuGmEVZGB4vCM2ALNvBDDKIZDnoAaK0209kviFLAV %25test%25/Maildir/cur user/group 1000 1000 dennis schafroth 16877 1415790417";
+    const char *testSIG = "SIG 9iNlOKBHPfeAtRpsCgaQqTwKuGmEVZGB4vCM2ALNvBDDKIZDnoAaK0209kviFLAV %25test%25/Maildir/cur user/group 1000 1000 dennis schafroth 16877 1415790417\n";
     char *buffer = strdup(testSIG);
     conn_remove_key(buffer);
     printf("'%s'\n", buffer);
