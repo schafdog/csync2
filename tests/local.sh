c3619fe32c6bcd234ca7d8072cad1116b109bbb7
Dennis schafroth <dennis@schafroth.com>
2015-01-23 11:00:48 +0100

    Testing on rsync result

--- tests/local.sh
+++ tests/local.sh
@@ -53,6 +53,7 @@ function cmd {
     echo "select filename from file order by filename; select peername,filename,operation,other from dirty order by filename;" | mysql -t -u csync2_$HOST -pcsync2_$HOST csync2_$HOST > ${TESTNAME}/${COUNT}.mysql 2> /dev/null
     testing ${TESTNAME}/${COUNT}.mysql
     rsync --delete -nHav test/local/ peer:`pwd`/test/peer/ |grep -v "bytes/sec" |grep -v "(DRY RUN)" |grep -v "sending incremental" > ${TESTNAME}/${COUNT}.rsync
+    testing ${TESTNAME}/${COUNT}.rsync
     echo "${COUNT}. END $CMD ${DESC}" 
     let COUNT=$COUNT+1
     ${PAUSE}
