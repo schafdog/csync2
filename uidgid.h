5408f431b1222b7e26091945a5794f22cac0a542
Dennis schafroth <dennis@schafroth.com>
2015-02-23 00:20:33 +0100

    change API

--- uidgid.h
+++ uidgid.h
@@ -3,8 +3,8 @@
 #define UIDGID_H
 
 uid_t name_to_uid(char const *name, gid_t *gid);
-char *uid_to_name(uid_t uid, char *buffer, int length);
-char *gid_to_name(gid_t gid, char *buffer, int length);
+int uid_to_name(uid_t uid, char *buffer, int length, const char *default_value);
+int gid_to_name(gid_t gid, char *buffer, int length, const char *default_value);
 uid_t name_to_gid(char const *name);
 
 #endif 
