26f66768d144f8fba82b77042551591a52cc2b83
Dennis schafroth <dennis@schafroth.com>
2015-06-13 08:38:09 +0200

    Use buffer to hold temporary strings

--- check.c
+++ check.c
@@ -21,6 +21,7 @@
 #include "csync2.h"
 #include "digest.h"
 #include "db_api.h"
+#include "buffer.h"
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -575,41 +576,41 @@ struct textlist *csync_check_link_move(const char *peername, const char *filenam
 
 void csync_check_dir(const char* file, int recursive, int init_run, int version, int dirdump_this, int flags)
 {
-  struct dirent **namelist;
-  int n = 0;
-  csync_debug(2, "Checking %s%s* ..\n", file, !strcmp(file, "/") ? "" : "/");
-  n = scandir(file, &namelist, 0, alphasort);
-  if (n < 0) {
-    csync_debug(0, "%s in scandir: %s (%s)\n", strerror(errno), file, file);
-    csync_error_count++;
-  } else {
-    while(n--) {
-      on_cygwin_lowercase(namelist[n]->d_name);
-      if ( strcmp(namelist[n]->d_name, ".") &&
-	   strcmp(namelist[n]->d_name, "..") ) {
-	char fn[strlen(file)+
-		strlen(namelist[n]->d_name)+2];
-	sprintf(fn, "%s/%s",
-		!strcmp(file, "/") ? "" : file,
-		namelist[n]->d_name);
-	char *operation;
-	if (csync_check_mod(fn, recursive, 0, init_run, version, &operation, flags))
-	  dirdump_this = 1;
-      }
-      free(namelist[n]);
+    struct dirent **namelist;
+    int n = 0;
+    csync_debug(2, "Checking %s%s* ..\n", file, !strcmp(file, "/") ? "" : "/");
+    n = scandir(file, &namelist, 0, alphasort);
+    if (n < 0) {
+	csync_debug(0, "%s in scandir: %s (%s)\n", strerror(errno), file, file);
+	csync_error_count++;
+    } else {
+	while(n--) {
+	    on_cygwin_lowercase(namelist[n]->d_name);
+	    if ( strcmp(namelist[n]->d_name, ".") &&
+		 strcmp(namelist[n]->d_name, "..") ) {
+		char fn[strlen(file)+
+			strlen(namelist[n]->d_name)+2];
+		sprintf(fn, "%s/%s",
+			!strcmp(file, "/") ? "" : file,
+			namelist[n]->d_name);
+		char *operation;
+		if (csync_check_mod(fn, recursive, 0, init_run, version, &operation, flags))
+		    dirdump_this = 1;
+	    }
+	    free(namelist[n]);
+	}
+	free(namelist);
     }
-    free(namelist);
-  }
-  if ( dirdump_this && csync_dump_dir_fd >= 0 ) {
-    int written = 0, len = strlen(file)+1;
-    while (written < len) {
-      int rc = write(csync_dump_dir_fd, file+written, len-written);
-      if (rc <= 0)
-	csync_fatal("Error while writing to dump_dir_fd %d: %s\n",
-		    csync_dump_dir_fd, strerror(errno));
-      written += rc;
+    if ( dirdump_this && csync_dump_dir_fd >= 0 ) {
+	int written = 0, len = strlen(file)+1;
+	while (written < len) {
+	    int rc = write(csync_dump_dir_fd, file+written, len-written);
+	    if (rc <= 0)
+		csync_fatal("Error while writing to dump_dir_fd %d: %s\n",
+			    csync_dump_dir_fd, strerror(errno));
+	    written += rc;
+	}
     }
-  }
 }
 
 static int update_dev_inode(struct stat *file_stat, const char *dev, const char *ino)
@@ -632,182 +633,180 @@ static int update_dev_inode(struct stat *file_stat, const char *dev, const char 
 
 void csync_file_check_mod(const char *file, struct stat *file_stat, int init_run, int version, char **operation)
 {
-  int this_is_dirty = 0;
-  int calc_digest = 0;
-  if (csync_compare_mode)
-    printf("%s\n", file);
-  char *checktxt = strdup(csync_genchecktxt_version(file_stat, file, SET_USER|SET_GROUP, version));
-  // Assume that this isn't a upgrade and thus same version
-  int is_upgrade = 0;
-  int db_version = version;
-  const char *encoded = db_encode(file);
-  //char *operation = 0;
-  char *other = 0; 
-  SQL_BEGIN("Checking File",
-	    "SELECT checktxt, inode, device, digest FROM file WHERE "
-	    "filename = '%s' ", encoded)
+    BUF_P buffer = buffer_init();
+    int this_is_dirty = 0;
+    int calc_digest = 0;
+    if (csync_compare_mode)
+	printf("%s\n", file);
+    char *checktxt = buffer_strdup(buffer, csync_genchecktxt_version(file_stat, file, SET_USER|SET_GROUP, version));
+    // Assume that this isn't a upgrade and thus same version
+    int is_upgrade = 0;
+    int db_version = version;
+    const char *encoded = db_encode(file);
+    //char *operation = 0;
+    char *other = 0; 
+    SQL_BEGIN("Checking File",
+	      "SELECT checktxt, inode, device, digest FROM file WHERE "
+	      "filename = '%s' ", encoded)
     {
-      db_version = csync_get_checktxt_version(SQL_V(0));
+	db_version = csync_get_checktxt_version(SQL_V(0));
 
-      if (db_version < 1 || db_version > 2) {
-		      csync_debug(0, "Error extracting version from checktxt: %s", SQL_V(0));
-      }
-      const char *checktxt_db = db_decode(SQL_V(0));
-      const char *checktxt_same_version = checktxt;
-      const char *inode    = SQL_V(1);
-      const char *device   = SQL_V(2);
-      const char *digest_p = SQL_V(3);
-      // const char *mtime  = SQL_V(3);
-      // const char *type   = SQL_V(4);
-      int flag = 0;
-      if (strstr(checktxt_db, ":user=") != NULL)
-	  flag |= SET_USER; 
-      if (strstr(checktxt_db, ":group=") != NULL)
-	  flag |= SET_GROUP; 
-      
-      if (update_dev_inode(file_stat, device, inode) ) {
-	  csync_debug(0, "File %s has changed device:inode %s:%s -> %llu:%llu\n", file, device, inode, file_stat->st_dev, file_stat->st_ino);
-	  is_upgrade = 1;
-      }
-      if (!digest_p && strstr(checktxt, "type=reg")) {
-	calc_digest = 1;
-	is_upgrade = 1;
-      }
-      if (db_version != version || flag != (SET_USER|SET_GROUP)) {
-	checktxt_same_version = csync_genchecktxt_version(file_stat, file, flag, db_version);
-	if (csync_cmpchecktxt(checktxt, checktxt_same_version))
-	  is_upgrade = 1;
-      }
-      if (csync_cmpchecktxt(checktxt_same_version, checktxt_db)) {
-	if (strstr(checktxt, "type=dir"))
-	    *operation = ringbuffer_strdup("MOD_DIR");
-	else
-	    *operation = ringbuffer_strdup("MOD");
-	csync_debug(2, "%s has changed: \n    %s \nDB: %s %s\n", file, checktxt_same_version, checktxt_db, *operation);
-	this_is_dirty = 1;
-      }
-    } SQL_FIN {
-    if ( SQL_COUNT == 0 ) {
-      csync_debug(2, "New file: %s\n", file);
-      if (S_ISREG(file_stat->st_mode)) {
-	*operation = ringbuffer_strdup("NEW");
-	calc_digest = 1; 
-      }
-      else if (S_ISDIR(file_stat->st_mode)) 
-	*operation = ringbuffer_strdup("MKDIR");
-      else if (S_ISCHR(file_stat->st_mode))
-	*operation = ringbuffer_strdup("MKCHR");
-      else if (S_ISBLK(file_stat->st_mode))
-	*operation = ringbuffer_strdup("MKBLK");
-      else if (S_ISFIFO(file_stat->st_mode))
-	*operation = ringbuffer_strdup("MKFIFO");
-      else if ( S_ISLNK(file_stat->st_mode) ) {
-	// TODO get max path
-	int max = 1024;
-	char target[max];
-	int len = readlink(file, target, max-1);
-	if (len > 0) {
-	  target[len] = 0;
-	  *operation = "MKLINK"; 
-	  other = strdup(target);
+	if (db_version < 1 || db_version > 2) {
+	    csync_debug(0, "Error extracting version from checktxt: %s", SQL_V(0));
 	}
-	else
-	  csync_debug(0, "Failed to read link on %s\n", file);
-      }
-      this_is_dirty = 1;
-    }
-  } SQL_END;
+	const char *checktxt_db = db_decode(SQL_V(0));
+	const char *checktxt_same_version = checktxt;
+	const char *inode    = SQL_V(1);
+	const char *device   = SQL_V(2);
+	const char *digest_p = SQL_V(3);
+	// const char *mtime  = SQL_V(3);
+	// const char *type   = SQL_V(4);
+	int flag = 0;
+	if (strstr(checktxt_db, ":user=") != NULL)
+	    flag |= SET_USER; 
+	if (strstr(checktxt_db, ":group=") != NULL)
+	    flag |= SET_GROUP; 
 
-  char *digest = NULL; 
-  if (calc_digest) {
-    int size = 2*DIGEST_MAX_SIZE+1;
-    digest = malloc(size);
-    int rc = dsync_digest_path_hex(file, "sha1", digest, size);
-    if (rc)
-      csync_debug(0, "Error generating digest: %s %d", digest, rc);
-  }
+	if (update_dev_inode(file_stat, device, inode) ) {
+	    csync_debug(0, "File %s has changed device:inode %s:%s -> %llu:%llu\n",
+			file, device, inode, file_stat->st_dev, file_stat->st_ino);
+	    is_upgrade = 1;
+	}
+	if (!digest_p && strstr(checktxt, "type=reg")) {
+	    calc_digest = 1;
+	    is_upgrade = 1;
+	}
+	if (db_version != version || flag != (SET_USER|SET_GROUP)) {
+	    checktxt_same_version = csync_genchecktxt_version(file_stat, file, flag, db_version);
+	    if (csync_cmpchecktxt(checktxt, checktxt_same_version))
+		is_upgrade = 1;
+	}
+	if (csync_cmpchecktxt(checktxt_same_version, checktxt_db)) {
+	    if (strstr(checktxt, "type=dir"))
+		*operation = buffer_strdup(buffer, "MOD_DIR");
+	    else
+		*operation = buffer_strdup(buffer, "MOD");
+	    csync_debug(2, "%s has changed: \n    %s \nDB: %s %s\n", file, checktxt_same_version, checktxt_db, *operation);
+	    this_is_dirty = 1;
+	}
+    } SQL_FIN {
+	if ( SQL_COUNT == 0 ) {
+	    csync_debug(2, "New file: %s\n", file);
+	    if (S_ISREG(file_stat->st_mode)) {
+		*operation = buffer_strdup(buffer, "NEW");
+		calc_digest = 1; 
+	    }
+	    else if (S_ISDIR(file_stat->st_mode)) 
+		*operation = buffer_strdup(buffer, "MKDIR");
+	    else if (S_ISCHR(file_stat->st_mode))
+		*operation = buffer_strdup(buffer, "MKCHR");
+	    else if (S_ISBLK(file_stat->st_mode))
+		*operation = buffer_strdup(buffer, "MKBLK");
+	    else if (S_ISFIFO(file_stat->st_mode))
+		*operation = buffer_strdup(buffer, "MKFIFO");
+	    else if ( S_ISLNK(file_stat->st_mode) ) {
+		// TODO get max path
+		int max = 1024;
+		char target[max];
+		int len = readlink(file, target, max-1);
+		if (len > 0) {
+		    target[len] = 0;
+		    *operation = buffer_strdup(buffer, "MKLINK"); 
+		    other = buffer_strdup(buffer, target);
+		}
+		else
+		    csync_debug(0, "Failed to read link on %s\n", file);
+	    }
+	    this_is_dirty = 1;
+	}
+    } SQL_END;
 
-  if ( (is_upgrade || this_is_dirty) && !csync_compare_mode ) {
-    int has_links = (file_stat->st_nlink > 1 && S_ISREG(file_stat->st_mode));
-    if (has_links) {
-      struct textlist *tl = NULL;
-      //csync_debug(2, "File %s has links: %d\n", file, file_stat->st_nlink);
-      // tl = csync_check_link(file, checktxt, file_stat, operation, csync_mark_hardlinks);
-      if (tl)
-	textlist_free(tl);
+    char *digest = NULL; 
+    if (calc_digest) {
+	int size = 2*DIGEST_MAX_SIZE+1;
+	digest = buffer_malloc(buffer, size);
+	int rc = dsync_digest_path_hex(file, "sha1", digest, size);
+	if (rc)
+	    csync_debug(0, "Error generating digest: %s %d", digest, rc);
     }
+    
+    if ( (is_upgrade || this_is_dirty) && !csync_compare_mode ) {
+	int has_links = (file_stat->st_nlink > 1 && S_ISREG(file_stat->st_mode));
+	if (has_links) {
+	    struct textlist *tl = NULL;
+	    //csync_debug(2, "File %s has links: %d\n", file, file_stat->st_nlink);
+	    // tl = csync_check_link(file, checktxt, file_stat, operation, csync_mark_hardlinks);
+	    if (tl)
+		textlist_free(tl);
+	}
 
-    unsigned long long dev = (file_stat->st_dev != 0 ? file_stat->st_dev : file_stat->st_rdev);
-    const char *checktxt_encoded = db_encode(checktxt);
-    if (is_upgrade) {
-      SQL("Update file entry",
-	  "UPDATE file set checktxt='%s', device=%lu, inode=%llu, digest=%s where filename = '%s'",
-	  checktxt_encoded, dev, file_stat->st_ino, csync_db_quote(digest), encoded);
+	unsigned long long dev = (file_stat->st_dev != 0 ? file_stat->st_dev : file_stat->st_rdev);
+	const char *checktxt_encoded = db_encode(checktxt);
+	if (is_upgrade) {
+	    SQL("Update file entry",
+		"UPDATE file set checktxt='%s', device=%lu, inode=%llu, digest=%s where filename = '%s'",
+		checktxt_encoded, dev, file_stat->st_ino, csync_db_quote(digest), encoded);
+	}
+	else {
+	    SQL("Deleting old file entry", "DELETE FROM file WHERE filename = '%s'", encoded);
+	    SQL("Adding or updating file entry",
+		"INSERT INTO file (filename, checktxt, device, inode, digest, mode, size) VALUES ('%s', '%s', %lu, %llu, %s, %u, %lu)",
+		encoded, 
+		checktxt_encoded,
+		dev,
+		file_stat->st_ino,
+		csync_db_quote(digest),
+		file_stat->st_mode, 
+		file_stat->st_size 
+		);
+	}
+	if (!init_run && this_is_dirty) {
+	    //      csync_debug(0, "check_dirty (mod): before mark (all) \n");
+	    char dev_str[100];
+	    char ino_str[100];
+	    sprintf(dev_str, DEV_FORMAT, file_stat->st_dev);
+	    sprintf(ino_str, INO_FORMAT, file_stat->st_ino);
+	    csync_mark_other(file, 0, 0, *operation,  checktxt_encoded, dev_str, ino_str, other);
+	}
     }
-    else {
-      SQL("Deleting old file entry", "DELETE FROM file WHERE filename = '%s'", encoded);
-      SQL("Adding or updating file entry",
-	  "INSERT INTO file (filename, checktxt, device, inode, digest, mode, size) VALUES ('%s', '%s', %lu, %llu, %s, %u, %lu)",
-	  encoded, 
-	  checktxt_encoded,
-	  dev,
-	  file_stat->st_ino,
-	  csync_db_quote(digest),
-	  file_stat->st_mode, 
-	  file_stat->st_size 
-	  );
-    }
-    if (!init_run && this_is_dirty) {
-      //      csync_debug(0, "check_dirty (mod): before mark (all) \n");
-      char dev_str[100];
-      char ino_str[100];
-      sprintf(dev_str, DEV_FORMAT, file_stat->st_dev);
-      sprintf(ino_str, INO_FORMAT, file_stat->st_ino);
-      csync_mark_other(file, 0, 0, *operation,  checktxt_encoded, dev_str, ino_str, other);
-      if (other) 
-	free(other);
-    }
-  }
-  if (digest)
-    free(digest);
-  free(checktxt);
+    buffer_destroy(buffer);
 }
 
 int csync_check_mod(const char *file, int recursive, int ignnoent, int init_run, int version, char **operation, int flags)
 {
-  int check_type = csync_match_file(file);
-  int dirdump_this = 0, dirdump_parent = MATCH_NONE;
-  int this_is_dirty = 0;
-  struct stat st;
+    int check_type = csync_match_file(file);
+    int dirdump_this = 0, dirdump_parent = MATCH_NONE;
+    int this_is_dirty = 0;
+    struct stat st;
   
-  if ( check_type>0 && lstat_strict(file, &st) != 0 ) {
-    if ( ignnoent ) 
-      return 0;
-    csync_debug(0, "check_mod: ERROR: Can't stat %s.\n", file);
-    // TODO verify what to return, since caller of csync_check_mod is only checking for non-zero.
-    // return ERROR;
-    return  MATCH_NONE;
-  }
+    if ( check_type>0 && lstat_strict(file, &st) != 0 ) {
+	if ( ignnoent ) 
+	    return 0;
+	csync_debug(0, "check_mod: ERROR: Can't stat %s.\n", file);
+	// TODO verify what to return, since caller of csync_check_mod is only checking for non-zero.
+	// return ERROR;
+	return  MATCH_NONE;
+    }
 
-  switch ( check_type ) 
+    switch ( check_type ) 
     {
     case MATCH_NEXT:
-      csync_file_check_mod(file, &st, init_run, version, operation);
-      dirdump_this = 1;
-      dirdump_parent = 1;
-      /* fall thru */
+	csync_file_check_mod(file, &st, init_run, version, operation);
+	dirdump_this = 1;
+	dirdump_parent = 1;
+	/* fall thru */
     case MATCH_INTO:
-      if ( !recursive ) 
+	if ( !recursive ) 
+	    break;
+	if ( !S_ISDIR(st.st_mode) ) 
+	    break;
+	csync_check_dir(file, recursive, init_run, version, dirdump_this, flags);
 	break;
-      if ( !S_ISDIR(st.st_mode) ) 
-	break;
-      csync_check_dir(file, recursive, init_run, version, dirdump_this, flags);
-      break;
     default:
-      csync_debug(2, "Don't check at all: %s\n", file);
-      break;
+	csync_debug(2, "Don't check at all: %s\n", file);
+	break;
     }
-  return dirdump_parent;
+    return dirdump_parent;
 }
 
 char *csync_check_recursive(const char *filename, int recursive, int init_run, int version, int flags)
