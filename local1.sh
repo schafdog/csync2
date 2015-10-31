169e0bb59d99337f57130d706531fb789146979c
Dennis Schafroth <dennis@schafroth.dk>
2015-10-31 11:32:52 +0100

    Remove local1.sh

--- local1.sh
+++
@@ -1,6 +0,0 @@
-echo "delete from dirty ; delete from file" | mysql -u csync2_local -pcsync2_local csync2_local
-./csync2 -p 30860 -K csync2_local.cfg -N local -cr$DEBUG test/local
-echo "select * from file;" | mysql -u csync2_local -pcsync2_local csync2_local > file.res
-./csync2 -p 30860 -K csync2_local.cfg -N local -ur$DEBUG test/local
-echo "select count(*) from dirty;" | mysql -N -u csync2_local -pcsync2_local csync2_local > empty.res
-
