135c15db2274d315750c36691d477d956638fcc6
Dennis schafroth <dennis@schafroth.com>
2016-07-26 20:47:29 +0200

    Use path of program to detect directory

--- make_install_branch.sh
+++ make_install_branch.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-cd /export/home/dennis/Projects/csync2/csync2
+CURRENT=`basedir $0`
+cd ${CURRENT}
 sudo -u dennis /bin/bash <<EOF
-cd /export/home/dennis/Projects/csync2/csync2
+cd ${CURRENT}
 if [ "$1" != "" ] ; then
 git checkout $1 || die
 fi
