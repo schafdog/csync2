1443214519c3f14f1a69857eab1a7d559f38c848
Dennis schafroth <dennis@schafroth.com>
2016-07-28 12:34:44 +0200

    Update to handle ssh self

--- make_install_branch.sh
+++ make_install_branch.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 CURRENT=`dirname $0`
 cd ${CURRENT}
-sudo -u dennis /bin/bash <<EOF
+if [ "$1" != "" ] ; then
+    sudo ssh $1 `pwd`/`basename $0`
+else
+    sudo -u dennis /bin/bash <<EOF
 cd ${CURRENT}
 if [ "$1" != "" ] ; then
 git checkout $1 || die
@@ -9,5 +12,7 @@ fi
 git pull
 make clean all
 EOF
-make install
-/usr/local/sbin/csync2 -V
+    make install
+    /usr/local/sbin/csync2 -V
+done
+
