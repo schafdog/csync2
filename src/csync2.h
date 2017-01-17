ac337441dcd24c7d5ccbbb2db428d967a83d524a
Dennis schafroth <dennis@schafroth.com>
2017-01-17 21:03:11 +0100

    Swap OP_NEW - OP_MKDIR

--- src/csync2.h
+++ src/csync2.h
@@ -262,8 +262,8 @@ int update_dev_inode(struct stat *file_stat, const char *dev, const char *ino);
 struct textlist;
 
 /* check.c */
-#define OP_NEW      1
-#define OP_MKDIR    2
+#define OP_MKDIR    1
+#define OP_NEW      2
 #define OP_MKFIFO   4
 #define OP_MKCHR    8
 #define OP_MOVE     16
