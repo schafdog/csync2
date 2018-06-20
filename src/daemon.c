036ae9b789a6400f0f9b8f959c581c933c3f1aec
Dennis schafroth <dennis@schafroth.com>
2018-06-20 22:18:23 +0200

    Attempt to fix path not found response

--- src/daemon.c
+++ src/daemon.c
@@ -862,7 +862,7 @@ int csync_daemon_sig(int conn, char *filename, char *tag[32], db_conn_p db, cons
     if ( lstat_strict(filename, &st) != 0) {
 	char *path;
 	if ((path = csync_check_path(filename))) {
-	    const char *otherfile = url_encode(prefixencode(filename));
+	    const char *otherfile = url_encode(prefixencode(path));
 	    conn_printf(conn, PATH_NOT_FOUND "%s\n", otherfile);
 	    return NEXT_CMD;
 	}
