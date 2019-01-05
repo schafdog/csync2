630af4d7be73ce911ff3a3651cd6a00aa85ed112
Dennis schafroth <dennis@schafroth.com>
2019-01-05 23:34:03 +0100

    Now also on dirty

--- directory_size.sh
+++ directory_size.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
 DIRECTORY=$1
+if [ "$1" == "" ] ; then
+    DIRECTORY=$PWD
+fi
+
 ORDER=$2
 if [ "$ORDER" == "" ] ; then
    ORDER=3
 fi
-DEPTH=$3
+
+TABLE=file
+if [ "$3" != "" ] ; then
+    TABLE=$3
+fi
+
+SUM_SIZE="sum(size)"
+if [ "$TABLE" == "dirty" ] ; then
+    SUM_SIZE="SUM(0)"
+fi
+
+DEPTH=$4
 CURDEPTH="LENGTH(SUBSTRING(filename, LENGTH('$DIRECTORY'))) - LENGTH(REPLACE(SUBSTRING(filename, LENGTH('$DIRECTORY')), '/', '')) - 1"
-MAXDEPTH=", max($CURDEPTH) "
+MAXDEPTH=", max($CURDEPTH) as maxdepth"
 if [ "$DEPTH" != "" ] ; then
     let DEPTH=$DEPTH
     DEPTH_LIMIT_SQL=" and $CURDEPTH < $DEPTH "
 fi 
 cat << __EOT__
-select substring(filename, 1, locate('/', filename, length('$DIRECTORY')+1)-1) as directory $MAXDEPTH, count(filename), sum(size) from file where filename like '$DIRECTORY%' $DEPTH_LIMIT_SQL group by directory order by $ORDER;
+SELECT substring(filename, 1, locate('/', filename, length('$DIRECTORY')+1)-1) as directory $MAXDEPTH,
+       count(filename),
+       $SUM_SIZE
+FROM $TABLE WHERE filename LIKE '$DIRECTORY%' $DEPTH_LIMIT_SQL 
+GROUP BY directory 
+ORDER BY $ORDER;
 __EOT__
