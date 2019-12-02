5f3c483817d43a66620b598ea44de9987eea0ed2
Dennis schafroth <dennis@schafroth.com>
2019-12-02 19:21:23 +0100

    Add redis layer

---
+++ src/redis.c
@@ -0,0 +1,77 @@
+
+#include "csync2.h"
+#include "redis.h"
+#include <hiredis/hiredis.h>
+#include <stdio.h>
+#include <time.h>
+
+redisContext *redis_context = NULL;
+redisReply *redis_reply = NULL;
+int isunix = 0;    
+
+int lock_time = 60;
+
+int csync_redis_connect(char *redis) {
+    if (redis == NULL)
+	return -1;
+    char *pos = strchr(redis, ':');
+    int port = 6379;
+    if (pos != NULL) {
+	*pos = 0;
+	int tmp_port = atoi(pos+1);
+	if (tmp_port > 0)
+	    port = tmp_port;
+    };
+    csync_debug(1, "Connecting to redis %s:%d\n", redis, port);
+    struct timeval timeout = { 1, 500000 }; // 1.5 seconds
+    if (isunix) {
+        redis_context = redisConnectUnixWithTimeout(redis, timeout);
+    } else {
+        redis_context = redisConnectWithTimeout(redis, port, timeout);
+    }
+    if (redis_context == NULL || redis_context->err) {
+        if (redis_context) {
+            csync_debug(0, "Connection error: %s\n", redis_context->errstr);
+            redisFree(redis_context);
+        } else {
+            csync_debug(0, "Connection error: can't allocate redis context\n");
+        }
+        return 1;
+    }
+    return 0;
+}
+
+time_t csync_redis_lock(filename_p filename) {
+    if (redis_context == NULL)
+	return 0;
+    time_t unix_time = time(NULL);
+    redis_reply = redisCommand(redis_context, "SET %s %d NX EX %d", filename, unix_time+lock_time, lock_time);
+    if (strcmp(redis_reply->str, "OK")) {
+	unix_time = -1;
+    }
+    freeReplyObject(redis_reply);
+    return unix_time;
+}
+
+void csync_redis_unlock(filename_p filename, time_t unix_time) {
+    if (redis_context == NULL)
+	return;
+    time_t now = time(NULL);
+    if (now > unix_time + lock_time) {
+	csync_debug(0, "operation took longer than lock time: %d (%d)", time - unix_time, lock_time);
+    } else {
+	redis_reply = redisCommand(redis_context, "DEL %s");
+	if (redis_reply->integer != 1)
+	    csync_debug(0, "redis_unlock failed to delete one key: %d\n", redis_reply->integer);
+	freeReplyObject(redis_reply);
+    }
+}
+
+int clear_redis_lock(filename_p filename, time_t lock_time, int status) {
+    csync_redis_unlock(filename, lock_time);
+    return status;
+}
+
+void csync_redis_close() {
+    redisFree(redis_context);
+}
