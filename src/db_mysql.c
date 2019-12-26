b8775ffd17d6fc296ffaec23eecc512ab55bd29e
Dennis Schafroth <dennis@schafroth.dk>
2019-12-27 00:30:09 +0100

    fix for common db

--- src/db_mysql.c
+++ src/db_mysql.c
@@ -394,7 +394,7 @@ int db_mysql_upgrade_to_schema(db_conn_p conn, int version)
 		 "  mtime     int    	   DEFAULT NULL,"
 		 "  type      int    	   DEFAULT NULL,"
 		 "  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,"
-		 "  UNIQUE KEY `filename_peer` (`filename`(%u),`peername`)"
+		 "  UNIQUE KEY `filename_peername_myname_` (`filename`(%u),`peername`,`myname`)"
 		 //		"  KEY `dirty_host` (`peername`(10))"
 		 ") ENGINE=InnoDB CHARACTER SET utf8 COLLATE utf8_bin", FILE_LENGTH, HOST_LENGTH, HOST_LENGTH, FILE_LENGTH, FILE_LENGTH);
 
