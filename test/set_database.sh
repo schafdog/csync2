57caf357ae7e13183bea9787b25609d2e2b61c0b
Dennis schafroth <dennis@schafroth.com>
2019-12-27 12:19:14 +0100

    Use file with database name

--- test/set_database.sh
+++ test/set_database.sh
@@ -2,5 +2,4 @@ if [ "$1" == "" ] ; then
     echo Database required: mysql, pgsql, fdbsql, cockroach
     exit
 fi
-ln -sf csync2_$1_local.cfg csync2_local.cfg
-ln -sf csync2_$1_peer.cfg  csync2_peer.cfg
+echo $1 > database
