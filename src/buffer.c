ba117ba3746c50fa02b0ba5c35b2cb013c7838d9
Dennis schafroth <dennis@schafroth.com>
2020-01-06 09:10:37 +0100

    Improve buffer_: handle resize and new API for get

--- src/buffer.c
+++ src/buffer.c
@@ -33,40 +33,59 @@ BUF_P buffer_init() {
     return buffer_init_size(BUFFER_LEN);
 };
 
-BUF_P buffer_init_size(int size) 
+void buffer_resize(BUF_P handle, int new_size);
+BUF_P buffer_init_size(int size)
 {
-    BUF_P handle  = malloc(sizeof(*handle));
-    handle->buffer = calloc(sizeof(*handle->buffer), size);
-    handle->size = size;
+    BUF_P handle = malloc(sizeof(*handle));
+    handle->buffer = NULL;
+    handle->size = 0;
     handle->current = 0;
+    buffer_resize(handle, size);
     return handle;
 };
 
+void buffer_resize(BUF_P handle, int new_size) {
+    void *old_buffer = handle->buffer;
+    int size = handle->size;
+    if (new_size <= size)
+	return;
+    handle->buffer = calloc(sizeof(*handle->buffer), new_size);
+    if (old_buffer) {
+	memcpy(handle->buffer, old_buffer, size*sizeof(*handle->buffer));
+	free(old_buffer);
+    }
+    handle->size = new_size;
+}
+
 void buffer_add(BUF_P handle, const char* string)
 {
-    if (handle->current < handle->size) {
+    if (handle) {
+	if (handle->current >= handle->size) {
+	    buffer_resize(handle, 2*(handle->size+1));
+	}
 	handle->buffer[handle->current] = (char*) string;
 	handle->current++;
     }
     else {
-	csync_fatal(0, "Buffer too small while adding %s", string);
+	csync_fatal(0, "buffer_add without handle %s", string);
     }
 };
 
 char *buffer_malloc(BUF_P handle, size_t length)
 {
-  char *out;
-  out = malloc(length);
-  out[0] = 0;
-
-  buffer_add(handle, out);
-  return out;
+    char *out = NULL;
+    if (handle) {
+	out = malloc(length);
+	out[0] = 0;
+	buffer_add(handle, out);
+    }
+    return out;
 };
 
 char *buffer_strdup(BUF_P handle, const char *cpy)
 {
   char *out = NULL;
-  if (cpy) {
+  if (handle && cpy) {
       out = strdup(cpy);
       buffer_add(handle, out);
   }
@@ -76,15 +95,24 @@ char *buffer_strdup(BUF_P handle, const char *cpy)
 char *buffer_quote(BUF_P handle, const char *cpy)
 {
   char *out = NULL;
-  if (cpy) {
+  if (handle && cpy) {
       out = malloc(strlen(cpy)+3);
       sprintf(out, "'%s'", cpy);
-      csync_log(LOG_DEBUG, 3, "QUOTE: %s => %s\n", cpy, out);
+      csync_log(LOG_DEBUG, 4, "QUOTE: %s => %s\n", cpy, out);
       buffer_add(handle, out);
       return out;
   }
   return "NULL";
 };
+
+char *buffer_printf(BUF_P handle, const char *format) {
+    char *line = "NULL";
+    if (handle) {
+	ASPRINTF(&line, "format");
+	buffer_add(handle, line);
+    }
+    return line;
+}
 
 void buffer_destroy(BUF_P handle) {
   int index;
@@ -99,6 +127,12 @@ void buffer_destroy(BUF_P handle) {
   free(handle);
 };
 
-int  buffer_getcount(BUF_P handle) {
+int  buffer_count(BUF_P handle) {
     return handle->current;
+};
+
+char *buffer_get(BUF_P handle, int index) {
+    if ( index < handle->current)
+	return handle->buffer[index];
+    return NULL;
 };
