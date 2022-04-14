01b7c73a49d5f3dbb3816664aabd6b0ca3d53717
Dennis schafroth <dennis@schafroth.com>
2022-04-14 12:54:31 +0200

    Remove ringbuffer. Add BUF_P parameter

--- src/urlencode.c
+++ src/urlencode.c
@@ -23,76 +23,61 @@
 #include <assert.h>
 #include <stdlib.h>
 
-#define RINGBUFF_LEN 100
-
-static char *ringbuff[RINGBUFF_LEN];
-static int ringbuff_counter = 0;
-
 // perl -e 'printf("\\%03o", $_) for(1..040)' | fold -64; echo
 static char badchars[] =
 	"\001\002\003\004\005\006\007\010\011\012\013\014\015\016\017\020"
 	"\021\022\023\024\025\026\027\030\031\032\033\034\035\036\037\040"
 	"\177\"'%$:|";
 
-const char *url_encode(const char *in)
+const char *url_encode(const char *in, BUF_P buffer)
 {
-	char *out;
-	int i, j, k, len;
+    char *out;
+    int i, j, k, len;
 
-	for (i=len=0; in[i]; i++, len++)
-		for (j=0; badchars[j]; j++)
-			if ( in[i] == badchars[j] ) { len+=2; break; }
+    for (i=len=0; in[i]; i++, len++)
+	for (j=0; badchars[j]; j++)
+	    if ( in[i] == badchars[j] ) { len+=2; break; }
 
-	out = malloc(len + 1);
+    out = buffer_malloc(buffer, len+1);
 
-	for (i=k=0; in[i]; i++) {
-		for (j=0; badchars[j]; j++)
-			if ( in[i] == badchars[j] ) break;
-		if ( badchars[j] ) {
-			snprintf(out+k, 4, "%%%02X", in[i]);
-			k += 3;
-		} else
-			out[k++] = in[i];
-	}
-	assert(k==len);
-	out[k] = 0;
+    for (i=k=0; in[i]; i++) {
+	for (j=0; badchars[j]; j++)
+	    if ( in[i] == badchars[j] ) break;
+	if ( badchars[j] ) {
+	    snprintf(out+k, 4, "%%%02X", in[i]);
+	    k += 3;
+	} else
+	    out[k++] = in[i];
+    }
+    assert(k==len);
+    out[k] = 0;
 
-	if ( ringbuff[ringbuff_counter] )
-		free(ringbuff[ringbuff_counter]);
-	ringbuff[ringbuff_counter++] = out;
-	if ( ringbuff_counter == RINGBUFF_LEN ) ringbuff_counter=0;
-
-	return out;
+    return out;
 }
 
-const char *url_decode(const char *in)
+const char *url_decode(const char *in, BUF_P buffer)
 {
-	char *out, num[3]="XX";
-	int i, k, len;
+    char *out, num[3]="XX";
+    int i, k, len;
 
-	if (in == NULL)
-	    return strdup("");
+    if (in == NULL)
+	return buffer_strdup(buffer, "");
 
-	for (i=len=0; in[i]; i++, len++)
-		if ( in[i] == '%' && in[i+1] && in[i+2] ) i+=2;
+    for (i=len=0; in[i]; i++, len++)
+	if ( in[i] == '%' && in[i+1] && in[i+2] ) i+=2;
 
-	out = malloc(len + 1);
+    out = buffer_malloc(buffer, len + 1);
 
-	for (i=k=0; in[i]; i++)
-		if ( in[i] == '%' && in[i+1] && in[i+2] ) {
-			num[0] = in[++i];
-			num[1] = in[++i];
-			out[k++] = strtol(num, 0, 16);
-		} else
-			out[k++] = in[i];
-	assert(k==len);
-	out[k] = 0;
+    for (i=k=0; in[i]; i++)
+	if ( in[i] == '%' && in[i+1] && in[i+2] ) {
+	    num[0] = in[++i];
+	    num[1] = in[++i];
+	    out[k++] = strtol(num, 0, 16);
+	} else
+	    out[k++] = in[i];
+    assert(k==len);
+    out[k] = 0;
 
-	if ( ringbuff[ringbuff_counter] )
-		free(ringbuff[ringbuff_counter]);
-	ringbuff[ringbuff_counter++] = out;
-	if ( ringbuff_counter == RINGBUFF_LEN ) ringbuff_counter=0;
-
-	return out;
+    return out;
 }
 
