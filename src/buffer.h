a9e5b6fe44f3a0de40f3e590507bc93081ce2b61
Dennis Schafroth <dennis@schafroth.com>
2024-12-05 15:05:39 +0100

    Indent and fix warning

--- src/buffer.h
+++ src/buffer.h
@@ -4,22 +4,22 @@
 /* buffer.c */
 
 struct buffer {
-    char ** buffer;
-    int size;
-    int current;
+	char **buffer;
+	int size;
+	int current;
 };
 
 typedef struct buffer *BUF_P;
 
-BUF_P  buffer_init();
-BUF_P  buffer_init_size(int size);
-char  *buffer_malloc(BUF_P handle, size_t length);
-char  *buffer_strdup(BUF_P handle, const char *cpy);
-char  *buffer_quote(BUF_P handle, const char *cpy);
+BUF_P buffer_init();
+BUF_P buffer_init_size(int size);
+char* buffer_malloc(BUF_P handle, size_t length);
+char* buffer_strdup(BUF_P handle, const char *cpy);
+char* buffer_quote(BUF_P handle, const char *cpy);
 // string is taken by buffer and release on destroy
-void   buffer_add(BUF_P handle, const char* string);
-void   buffer_destroy(BUF_P handle);
-int    buffer_count(BUF_P handle);
-char  *buffer_get(BUF_P handle, int index);
+void buffer_add(BUF_P handle, const char *string);
+void buffer_destroy(BUF_P handle);
+int buffer_count(BUF_P handle);
+char* buffer_get(BUF_P handle, int index);
 
 #endif
