1fe84ad5ebd48aaa81a80eafb8448ca9fa0252a7
Dennis Schafroth <dennis@schafroth.com>
2024-12-05 19:27:03 +0100

    Format. Spelling

--- src/buffer.c
+++ src/buffer.c
@@ -24,106 +24,104 @@
 #include <stdlib.h>
 #include "buffer.h"
 
-/* Maximum simultanous allocated */
+/* Maximum simultaneous allocated */
 #define BUFFER_LEN 100
 
-typedef void (*free_fn_t) (void *);
+typedef void (*free_fn_t)(void*);
 
 BUF_P buffer_init() {
-    return buffer_init_size(BUFFER_LEN);
-};
+	return buffer_init_size(BUFFER_LEN);
+}
+;
 
 void buffer_resize(BUF_P handle, int new_size);
 
-BUF_P buffer_init_size(int size)
-{
-    BUF_P handle = malloc(sizeof(*handle));
-    handle->size = size;
-    handle->buffer = calloc(size, sizeof(void*));
-    handle->current = 0;
-    return handle;
-};
+BUF_P buffer_init_size(int size) {
+	BUF_P handle = malloc(sizeof(*handle));
+	handle->size = size;
+	handle->buffer = calloc(size, sizeof(void*));
+	handle->current = 0;
+	return handle;
+}
+;
 
 void buffer_resize(BUF_P handle, int new_size) {
-    void *old_buffer = handle->buffer;
-    int size = handle->size;
-    if (new_size <= size)
-	return;
-    handle->buffer = calloc(new_size, sizeof(*handle->buffer));
-    if (old_buffer) {
-	memcpy(handle->buffer, old_buffer, size*sizeof(*handle->buffer));
-	free(old_buffer);
-    }
-    handle->size = new_size;
+	void *old_buffer = handle->buffer;
+	int size = handle->size;
+	if (new_size <= size)
+		return;
+	handle->buffer = calloc(new_size, sizeof(*handle->buffer));
+	if (old_buffer) {
+		memcpy(handle->buffer, old_buffer, size * sizeof(*handle->buffer));
+		free(old_buffer);
+	}
+	handle->size = new_size;
+}
+;
+
+void buffer_add(BUF_P handle, const char *string) {
+	if (handle) {
+		if (handle->current >= handle->size) {
+			buffer_resize(handle, 2 * (handle->size + 1));
+		}
+		handle->buffer[handle->current] = (char*) string;
+		handle->current++;
+	} else {
+		csync_fatal(0, "buffer_add without handle %s", string);
+	}
+};
+
+char* buffer_malloc(BUF_P handle, size_t length) {
+	char *out = NULL;
+	if (handle) {
+		out = malloc(length);
+		out[0] = 0;
+		buffer_add(handle, out);
+	}
+	return out;
 }
 
-void buffer_add(BUF_P handle, const char* string)
-{
-    if (handle) {
-	if (handle->current >= handle->size) {
-	    buffer_resize(handle, 2*(handle->size+1));
+char* buffer_strdup(BUF_P handle, const char *cpy) {
+	char *out = NULL;
+	if (handle && cpy) {
+		out = strdup(cpy);
+		buffer_add(handle, out);
 	}
-	handle->buffer[handle->current] = (char*) string;
-	handle->current++;
-    }
-    else {
-	csync_fatal(0, "buffer_add without handle %s", string);
-    }
-};
+	return out;
+}
 
-char *buffer_malloc(BUF_P handle, size_t length)
-{
-    char *out = NULL;
-    if (handle) {
-	out = malloc(length);
-	out[0] = 0;
-	buffer_add(handle, out);
-    }
-    return out;
-};
-
-char *buffer_strdup(BUF_P handle, const char *cpy)
-{
-  char *out = NULL;
-  if (handle && cpy) {
-      out = strdup(cpy);
-      buffer_add(handle, out);
-  }
-  return out;
-};
-
-char *buffer_quote(BUF_P handle, const char *cpy)
-{
-  char *out = NULL;
-  if (handle && cpy) {
-      out = malloc(strlen(cpy)+3);
-      sprintf(out, "'%s'", cpy);
-      csync_log(LOG_DEBUG, 4, "QUOTE: %s => %s\n", cpy, out);
-      buffer_add(handle, out);
-      return out;
-  }
-  return "NULL";
-};
+char* buffer_quote(BUF_P handle, const char *cpy) {
+	char *out = NULL;
+	if (handle && cpy) {
+		out = malloc(strlen(cpy) + 3);
+		sprintf(out, "'%s'", cpy);
+		csync_log(LOG_DEBUG, 4, "QUOTE: %s => %s\n", cpy, out);
+		buffer_add(handle, out);
+		return out;
+	}
+	return "NULL";
+}
 
 void buffer_destroy(BUF_P handle) {
-  int index;
-  
-  for (index = 0; index < handle->current; index++) {
-    if (handle->buffer[index]) {
-      free(handle->buffer[index]);
-      handle->buffer[index] = 0;
-    }
-  }
-  free(handle->buffer);
-  free(handle);
-};
+	int index;
 
-int  buffer_count(BUF_P handle) {
-    return handle->current;
-};
+	for (index = 0; index < handle->current; index++) {
+		if (handle->buffer[index]) {
+			free(handle->buffer[index]);
+			handle->buffer[index] = 0;
+		}
+	}
+	free(handle->buffer);
+	free(handle);
+}
 
-char *buffer_get(BUF_P handle, int index) {
-    if ( index < handle->current)
-	return handle->buffer[index];
-    return NULL;
-};
+int buffer_count(BUF_P handle) {
+	return handle->current;
+}
+
+char* buffer_get(BUF_P handle, int index) {
+	if (index < handle->current)
+		return handle->buffer[index];
+	return NULL;
+}
+
