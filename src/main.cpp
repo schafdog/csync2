5c29feb33e7567636d198e97855f674baab71c66
Dennis Schafroth <dennis@schafroth.com>
2025-07-08 18:27:56 +0200

    Make it easier to make tests

---
+++ src/main.cpp
@@ -0,0 +1,6 @@
+/*  -*- c-file-style: "k&r"; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: t -*- */
+#include "csync2.hpp"
+
+int  main(int argc, char **argv) {
+    return csync2_main(argc, argv);
+}
