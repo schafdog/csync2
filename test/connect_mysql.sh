0a571dbd6e4175694622cd1d0c6d711002b556c5
Dennis Schafroth <dennis@schafroth.com>
2025-08-04 13:38:42 +0200

    Seems to work in clear script

--- test/connect_mysql.sh
+++ test/connect_mysql.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 HOST=$1
-echo "connect mysql $HOST"
 mysql --defaults-extra-file=./$HOST.cnf --protocol tcp --table -h 127.0.0.1 -u csync2_$HOST csync2_$HOST
-echo "END connect mysql $HOST"
