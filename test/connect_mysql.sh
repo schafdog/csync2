c0574fb414f09d339b3e73808c957dd57e666aea
Dennis Schafroth <dennis@schafroth.com>
2025-08-04 12:51:41 +0200

    Still problem with password

--- test/connect_mysql.sh
+++ test/connect_mysql.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 HOST=$1
-mysql --defaults-extra-file=./$HOST.cnf --protocol tcp --table -h localhost -u csync2_$HOST csync2_$HOST
+echo "connect mysql $HOST"
+mysql --defaults-extra-file=./$HOST.cnf --protocol tcp --table -h 127.0.0.1 -u csync2_$HOST csync2_$HOST
+echo "END connect mysql $HOST"
