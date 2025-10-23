d7d9c92eb320419e1651b9f8e599d9b702249f05
Dennis Schafroth <dennis@schafroth.com>
2025-10-23 11:19:50 +0200

    Portable shasum

--- test/source_hash.sh
+++ test/source_hash.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-find src/ -maxdepth 1 \( -name "*.l" -o -name "*.y" -o -name "*.cpp" -o -name "*.hpp" \) -type f | sort | xargs -r cat | sha256sum | cut -d' ' -f1
+if command -v sha256sum >/dev/null 2>&1; then
+    SHASUM=sha256sum
+elif command -v shasum >/dev/null 2>&1; then
+    SHASUM="shasum -a 256"
+else
+    echo "No SHA256 tool found"
+    exit 1
+fi
+
+find src/ -maxdepth 1 \( -name "*.l" -o -name "*.y" -o -name "*.cpp" -o -name "*.hpp" \) -type f \
+    | sort | xargs -r cat | $SHASUM | cut -d' ' -f1
