35f1d4f8e86a1c73afa80f422d8f1266d5e5016c
Dennis Schafroth <dennis@schafroth.com>
2025-10-23 13:41:23 +0200

    Add architechure to hash

--- test/combine_hash.sh
+++ test/combine_hash.sh
@@ -3,4 +3,5 @@ OS=$1
 SOURCES_HASH=$2
 CONFIG_HASH=$3
 DEPS_HASH=$4
-echo "build-${OS}-$(echo "${SOURCES_HASH}-${CONFIG_HASH}-${DEPS_HASH}" | ./test/portable_hash.sh | cut -d' ' -f1 | head -c 16)"
+ARCH=$(uname -m)
+echo "build-${OS}-$ARCH-$(echo "${SOURCES_HASH}-${CONFIG_HASH}-${DEPS_HASH}" | ./test/portable_hash.sh | cut -d' ' -f1 | head -c 16)"
